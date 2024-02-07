1ba5313871ccae4ed97ed2b09065c7bad2516c21
yeontea <kopbm040608@gmail.com>
2024-02-07 15:40:19 +0900

    chipichipi

--- Board/src/main/java/com/green/Board/BoardApplication.java
+++ Board/src/main/java/com/green/Board/BoardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
+		System.out.println(123);
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
